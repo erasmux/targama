a860dff69f0b4e0eabbb6ad9e3e32f3ad1870378
Eran Mizrahi <erasmux@gmail.com>
2016-09-22 11:38:39 +0300

    set default locale to hebrew

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    opt = {}
+    opt[:locale] = I18n.locale unless I18n.locale == I18n.default_locale
+    opt
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Targama
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.fallbacks = [:en]
+    config.i18n.default_locale = :he
   end
 end
