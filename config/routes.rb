1cefca05af09ad44726ca0b81fed720a63b44c10
Eran Mizrahi <erasmux@gmail.com>
2016-09-24 16:31:38 +0300

    add home page (under construction for now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :dialects, :only => [:index, :create, :update, :show, :destroy]
   resources :gram_forms, :only => [:index, :create, :update, :show, :destroy]
   resources :twords
 
-  root 'twords#index'
+  root 'home#index'
 end
